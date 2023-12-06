0c25db08a8a750aa5b2026ca308b350cb62e6343
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-12-06 05:14:20 +0200

    Add files via upload

---
+++ 02. Gauss' Trick/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _02.Gauss__Trick
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> result = new List<int>();
+
+            if (numbers.Count % 2 == 0)
+            {
+                for (int i = 0; i < numbers.Count / 2; i++)
+                {
+                    result.Add(numbers[i] + numbers[numbers.Count - 1 - i]);
+                }
+            }
+            else if (numbers.Count % 2 != 0)
+            {
+                for (int i = 0; i < numbers.Count / 2; i++)
+                {
+                    result.Add(numbers[i] + numbers[numbers.Count - 1 - i]);
+                }
+
+                result.Add(numbers[numbers.Count / 2]);
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
