0c12a7d582aad18cede36d0f6172bd22f57343db
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-12-30 12:19:29 +0200

    Add files via upload

---
+++ 04. List of Products/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _04.List_Of_Products
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfProducts = int.Parse(Console.ReadLine());
+
+            List<string> products = new List<string>();
+
+            for (int i = 1; i <= numberOfProducts; i++)
+            {
+                string currentProduct = Console.ReadLine();
+                products.Add(currentProduct);
+            }
+
+            products.Sort();
+            
+            for (int i = 0; i < products.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}.{products[i]}");
+            }
+        }
+    }
+}
