3218661d977b24e2b09cdba0e721050e4cee7d89
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-12-30 12:21:42 +0200

    Add files via upload

---
+++ 06. List Manipulation Basics/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _06.List_Manipulation_Basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                string[] cmdInfo = command.Split();
+
+                if (cmdInfo[0] == "Add")
+                {
+                    int number = int.Parse(cmdInfo[1]);
+                    numbers.Add(number);
+                }
+                else if (cmdInfo[0] == "Remove")
+                {
+                    int number = int.Parse(cmdInfo[1]);
+                    numbers.Remove(number);
+                }
+                else if (cmdInfo[0] == "RemoveAt")
+                {
+                    int index = int.Parse(cmdInfo[1]);
+                    numbers.RemoveAt(index);
+                }
+                else if (cmdInfo[0] == "Insert")
+                {
+                    int number = int.Parse(cmdInfo[1]);
+                    int index = int.Parse(cmdInfo[2]);
+                    numbers.Insert(index, number);
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
