12f1e88a0809033b91b82d1ada8f80d5e5f18774
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-12-30 12:20:32 +0200

    Add files via upload

---
+++ 05. Remove Negatives and Reverse/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _05.Remove_Negatives_And_Reverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            numbers.RemoveAll(x => x < 0);
+            numbers.Reverse();
+
+            if (numbers.Count > 0)
+            {
+                Console.WriteLine(string.Join(" ", numbers));
+            }
+            else
+            {
+                Console.WriteLine("empty");
+            }
+        }
+    }
+}
